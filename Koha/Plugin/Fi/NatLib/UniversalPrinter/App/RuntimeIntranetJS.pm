018830756d867387dbb7b082619c3bf6e8d5c147
Slava Shishkin <slavashishkin@gmail.com>
2024-04-22 18:48:49 +0300

    WIP: MWP print a report

--- Koha/Plugin/Fi/NatLib/UniversalPrinter/App/RuntimeIntranetJS.pm
+++ Koha/Plugin/Fi/NatLib/UniversalPrinter/App/RuntimeIntranetJS.pm
@@ -11,6 +11,38 @@ sub runtime_intranet_js {
 
     return q|<script>
 
+        function attach_self_to_reporttable() {
+            var reportTable = $('#sql_output'); // Find the element with ID 'sql_output'
+            var button = $('<button>').text('Send to Universal Printer plugin');
+
+            button.on('click', function() {
+                var report_number = $('.report_number').text(); // Get report number from the text
+                var form = $('<form>').attr({
+                    method: 'POST',
+                    action: '/cgi-bin/koha/plugins/run.pl'
+                }).css({
+                    display: 'none'
+                });
+
+                console.log('report_number', report_number);
+
+                // Add hidden inputs to the form
+                $('<input>').attr({ type: 'hidden', name: 'class', value: '|.$self->{plugin}{metadata}{class}.q|' }).appendTo(form);
+                $('<input>').attr({ type: 'hidden', name: 'method', value: 'report' }).appendTo(form);
+                $('<input>').attr({ type: 'hidden', name: 'action', value: 'print_reports' }).appendTo(form);
+                $('<input>').attr({ type: 'hidden', name: 'report_id', value: report_number }).appendTo(form); // Add report's number as 'report_id'
+
+                $('body').append(form);
+                form.submit();
+            });
+
+            reportTable.before(button);
+        }
+
+
+        $(function() {
+            attach_self_to_reporttable();
+        });
     </script>|;
 }
 
