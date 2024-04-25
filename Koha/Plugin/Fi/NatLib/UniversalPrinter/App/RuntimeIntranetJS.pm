05962545f46db1aac2b0b210781c7f2e35521296
Slava Shishkin <slavashishkin@gmail.com>
2024-04-25 18:01:16 +0300

    Add button Send to Universal Printer plugin to "Holds queue" table

--- Koha/Plugin/Fi/NatLib/UniversalPrinter/App/RuntimeIntranetJS.pm
+++ Koha/Plugin/Fi/NatLib/UniversalPrinter/App/RuntimeIntranetJS.pm
@@ -11,47 +11,76 @@ sub runtime_intranet_js {
 
     return q|<script>
 
-        function attach_self_to_reporttable() {
-            var report_table = $('#sql_output'); // Find the element with ID 'sql_output'
-            var button = $('<button>').text('Send to Universal Printer plugin');
+            function attachSelfToReporttable(reportTableId) {
+                var reportTable = $('#' + reportTableId); // Use the passed ID to select the element
+                var button = $('<button>').text('Send to Universal Printer plugin').css({
+                        padding: '5px',
+                        margin: '0 0 5px 0'
+                    });
 
-            button.on('click', function() {
-                var url_params = new URLSearchParams(window.location.search);
-                var form = $('<form>').attr({
-                    method: 'POST',
-                    action: '/cgi-bin/koha/plugins/run.pl'
-                }).css({
-                    display: 'none'
-                });
+                button.on('click', function() {
+                    const urlParams = new URLSearchParams(window.location.search);
+                    var form = $('<form>').attr({
+                        method: 'POST',
+                        action: '/cgi-bin/koha/plugins/run.pl'
+                    }).css({
+                        display: 'none'
+                    });
 
-                // Add required fields inputs to the form
-                $('<input>').attr({ type: 'hidden', name: 'class', value: '|.$self->{plugin}{metadata}{class}.q|' }).appendTo(form);
-                $('<input>').attr({ type: 'hidden', name: 'method', value: 'report' }).appendTo(form);
-                $('<input>').attr({ type: 'hidden', name: 'action', value: 'print_reports' }).appendTo(form);
+                    // Determine the action for the form
+                    var actionValue = reportTableId === 'toolbar' ? 'print_reports' : 'print_holdsqueue';
 
-                // Select allowed URL parameters and add them as hidden fields to the form
-                const allowed_params = ['reports', 'param_name', 'sql_params'];
-                url_params.forEach(function(value, key) {
-                    if (allowed_params.includes(key)) {
-                        $('<input>').attr({
-                            type: 'hidden',
-                            name: key,
-                            value: value
-                        }).appendTo(form);
+                    // Add additional parameters for holdst_wrapper from URL, if needed
+                    $('<input>').attr({ type: 'hidden', name: 'class', value: '|.$self->{plugin}{metadata}{class}.q|' }).appendTo(form);
+                    $('<input>').attr({ type: 'hidden', name: 'method', value: 'report' }).appendTo(form);
+                    $('<input>').attr({ type: 'hidden', name: 'action', value: actionValue }).appendTo(form);
+
+                    // Add additional parameters for holdst_wrapper from URL, if needed
+                    if (reportTableId === 'holdst_wrapper') {
+                        ['branchlimit', 'itemtypeslimit', 'ccodeslimit', 'locationslimit'].forEach(function(param) {
+                            $('<input>').attr({ type: 'hidden', name: param, value: urlParams.get(param) }).appendTo(form);
+                        });
                     }
+
+                    // Add allowed URL parameters to the form
+                    const allowed_params = ['reports', 'param_name', 'sql_params'];
+                    urlParams.forEach(function(value, key) {
+                        if (allowed_params.includes(key)) {
+                            $('<input>').attr({
+                                type: 'hidden',
+                                name: key,
+                                value: value
+                            }).appendTo(form);
+                        }
+                    });
+                    $('body').append(form);
+                    form.submit();
                 });
 
-                $('body').append(form);
-                form.submit();
-            });
-
-            report_table.before(button);
+                if (reportTableId === 'holdst_wrapper') {
+                    reportTable.find('.dt-buttons').append(button); // Add button to dt-buttons div
+                } else if (reportTableId === 'toolbar') {
+                    $('#toolbar').append(button); // Add button to the toolbar div
+                }
         }
 
+        $(function() {
+            // Determine the page and element ID based on the URL
+            var path = window.location.pathname;
+            var urlParams = new URLSearchParams(window.location.search);
+            var phase = urlParams.get('phase');
+            var targetId = path.includes('view_holdsqueue.pl') ? 'holdst_wrapper' :
+                           (path.includes('guided_reports.pl') && phase === 'Run this report') ? 'toolbar' : undefined;
+
+            if (targetId) {
+                attachSelfToReporttable(targetId);
+            }
+        });
 
         $(function() {
-            attach_self_to_reporttable();
+            attachSelfToReporttable();
         });
+
     </script>|;
 }
 
